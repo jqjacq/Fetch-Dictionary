d61c1a8ebc9eda5af5f2b41b600e5a543723a508
Jacqueline Xu <85534903+jqjacq@users.noreply.github.com>
2023-04-11 22:56:20 -0400

    add fade effect& default display

--- script.js
+++ script.js
@@ -3,45 +3,61 @@
 
 const userInput = document.querySelector('.userInput');
 const submitButton = document.querySelector('.submitButton');
+const resultContainer = document.querySelector('.result-container')
 const displayWord = document.querySelector('.userWord');
 const displayDefinition = document.querySelector('.result');
 
-
+const wait = function () {
+    return new Promise(function (resolve) {
+        resultContainer.style.opacity = "1";
+      setTimeout(resolve, 2000);
+    });
+  };
+const defaultWord = function() {
+    wait()
+    displayWord.textContent = 'Welcome';
+    displayDefinition.innerHTML = `1. The act of greeting someone’s arrival, especially by saying "Welcome!"; reception. <br>
+    2. To affirm or greet the arrival of someone, especially by saying "Welcome!". <br>
+    3. Whose arrival is a cause of joy; received with gladness; admitted willingly to the house, entertainment, or company. <br>
+    4. Greeting given upon someone's arrival.`
+}
 async function getWordData(word) {
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
     const data = await response.json();
     return data;
-// try {
-//     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-//     if (!response.ok) throw new Error('Networking not responsing, try again.');
-//     return await response.json();
-// } catch (error) {
-//     console.error(`A chicken ate the definition, try again.`, error);
-//     }
 }
 
-submitButton.addEventListener('click', async function (e) {
-    e.preventDefault();
+async function getDefinition() {
     const word = userInput.value;
     const data = await getWordData(word);
-    
-    if (Array.isArray(data)) {
-     getWordData(word).then(data => {
-         const definitions = data[0].meanings.map(meaning => meaning.definitions[0].definition);
 
-          let definitionList = '';
-          definitions.forEach(
-              definition => {
-                  definitionList += `<li>${definition}</li>`;
-              });
-              displayDefinition.innerHTML = definitionList;
-              displayWord.textContent = word;
-          });
+    if (Array.isArray(data)) {
+        getWordData(word).then(data => {
+            const definitions = data[0].meanings.map(meaning => meaning.definitions[0].definition);
+            
+            let definitionList = '';
+            definitions.forEach(
+                definition => {
+                    definitionList += `<li>${definition}</li>`;
+                });
+                displayDefinition.innerHTML = definitionList;
+                displayWord.textContent = word;
+            });
         } else {
             displayWord.textContent = `The word '${word}' does not exist.`;
             displayDefinition.innerHTML = "This word does not exist in our dictionary. Otherwise, it may be a misspelling. Please try another word. "
         }
-    });
+    }
+
+submitButton.addEventListener('click', async function (e) {
+    resultContainer.style.opacity = "0";
+    wait();
+    e.preventDefault();
+    getDefinition();
+});
+
+defaultWord();
+
 //Using just fetch
 // const form = document.querySelector('form');
     // form.addEventListener('submit', function(e) {
